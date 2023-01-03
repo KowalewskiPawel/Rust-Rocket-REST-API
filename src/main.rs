5e1db78feecec3ecc286b6511fe2e4f7811f3f5b
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-01-03 17:25:01 +0100

    add CORS

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
 #[macro_use] extern crate rocket;
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+
+pub struct CORS;
 
 mod appconfig;
 mod apiv1;
 
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Attaching CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     appconfig::check_dbfile(appconfig::DATABASE_FILE);
-    rocket::build().mount("/api/", routes![apiv1::sayhi, apiv1::query, apiv1::web_create, apiv1::query_all])
+    rocket::build().attach(CORS).mount("/api/", routes![apiv1::sayhi, apiv1::query, apiv1::web_create, apiv1::query_all])
 }
